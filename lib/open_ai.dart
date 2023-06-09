bec4d9094cf6ae59ecc9d84658868d87f872267d
April Cheng <acheng118@my.bcit.ca>
2023-06-09 15:38:29 -0700

    Call to openAI API

---
+++ lib/open_ai.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart' show debugPrint;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'api_keys.dart';
+
+
+Future<String> callOpenAI(List<String> promptContent) async {
+  // API key stored in Flutter Secure Storage
+  final storage = FlutterSecureStorage();
+
+  // API endpoint and parameters
+  String apiUrl = 'https://api.openai.com/v1/chat/completions';
+  String? apiKey = await storage.read(key: 'api_key') ?? openAiApiKey; //get apikey from secure storage
+  debugPrint("HERE IS THE API KEY: $apiKey");
+  if (apiKey == null) {
+    throw Exception('API key not found');
+  }
+  // String apiKey = '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';
+  String prompt = 'Tell me about Sir Winston Churchill';
+
+  // Request payload
+  Map<String, dynamic> data = {
+    'model': 'gpt-3.5-turbo',
+    'messages': [
+      {'role': 'user', 'content': prompt},
+    ],
+  };
+
+  // Encode the data as JSON
+  String requestBody = jsonEncode(data);
+
+  // API request
+  http.Response response = await http.post(
+    Uri.parse(apiUrl),
+    headers: {
+      'Authorization': 'Bearer $apiKey',
+      'Content-Type': 'application/json',
+    },
+    body: requestBody,
+  );
+
+  // Parse the response
+  if (response.statusCode == 200) {
+    Map<String, dynamic> responseData = jsonDecode(response.body);
+    String completionText = responseData['choices'][0]['message']['content'];
+    debugPrint('Successful Response: ${response.statusCode}\nResponse: ${response.body}\nHeaders: ${response.headers}');
+    return completionText;
+  } else {
+    throw Exception('Failed to call OpenAI API: ${response.statusCode}\nResponse: ${response.body}\nHeaders: ${response.headers}');
+  }
+}
